4e4cec14547c59900938e2fda17df21179ecae5d
Bernardo <bernardo@ficafrio.com>
2026-06-11 05:59:15 -0300

    feat(ui): standby fiel ao 3310 como home

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,19 +2,18 @@
 #include <U8g2lib.h>
 #include "pins.h"
 #include "shell.h"
-#include "clockfmt.h"
 #include "drivers/buzzer.h"
 #include "drivers/buttons.h"
-#include "ui/statusbar.h"
 #include "ui/boot_anim.h"
 #include "ui/launcher_view.h"
+#include "apps/app_standby.h"
 #include "apps/app_clock.h"
 #include "apps/app_tones.h"
 #include "apps/app_about.h"
 
 static U8G2_PCD8544_84X48_F_4W_HW_SPI u8g2(U8G2_R0, PIN_LCD_CE, PIN_LCD_DC, PIN_LCD_RST);
 static Shell shell;
-static const App* kApps[] = {&app_tones, &app_about};
+static const App* kApps[] = {&app_clock, &app_tones, &app_about};
 
 void setup() {
   Serial.begin(115200);
@@ -25,7 +24,7 @@ void setup() {
   buzzer::init();
   buttons::init();
   boot_anim_play(u8g2);              // logo NOKIA + maos + startup chime (segura ate o fim)
-  shell.init(&app_clock, kApps, sizeof(kApps) / sizeof(kApps[0]));
+  shell.init(&app_standby, kApps, sizeof(kApps) / sizeof(kApps[0]));
 }
 
 void loop() {
@@ -42,12 +41,7 @@ void loop() {
   static uint32_t last_render = 0;
   if (now - last_render >= 50) {                // ~20 fps
     last_render = now;
-    char hhmm[6]; bool colon;
-    clock_format(now, hhmm, &colon);
-    if (!colon) hhmm[2] = ' ';
-
     u8g2.clearBuffer();
-    statusbar_draw(u8g2, hhmm, false);          // wifi off na F1
     if (shell.screen() == Shell::LAUNCHER) launcher_view_draw(u8g2, shell);
     else if (shell.active() && shell.active()->on_render)
       shell.active()->on_render(&u8g2);
